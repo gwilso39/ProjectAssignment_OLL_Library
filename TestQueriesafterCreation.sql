97d96c685c20263233a4c6f0d7e36cf786db0f14
Greg Wilson <greg.wilson5@outlook.com>
2018-02-24 22:08:06 -0500

    Have more test queries in a separate testqueriesaftercreation SQL file

--- TestQueriesafterCreation.sql
+++ TestQueriesafterCreation.sql
@@ -23,3 +23,21 @@ SELECT *
     JOIN Access as A
 	  ON a.accessid = u.accessid
   WHERE a.accessid > 2;
+
+SELECT *
+	FROM Books
+	WHERE genre = 'History';
+
+SELECT u.fname, u.lname, a.accesslevel
+	FROM Users as U
+	  JOIN Access as A
+	    ON A.accessid = U.accessid
+	WHERE accesslevel = 'Student';
+
+SELECT b.title, l.locationdescription, ls.lexile, ls.grade
+  FROM Books as b		
+    JOIN Locations as l
+	  on l.locationid = b.locationid
+	Join LexileScores as ls
+	  on ls.lexileid = b.lexilelower
+  WHERE ls.lexile > 500;
